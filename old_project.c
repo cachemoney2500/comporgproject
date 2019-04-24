6314363a569478f37eb00d8024d085a1c0e25837
christinekoul <46358805+christinekoul@users.noreply.github.com>
2019-04-24 11:48:49 -0400

    Update old_project.c

--- old_project.c
+++ old_project.c
@@ -287,7 +287,7 @@ void simulation(char input[5][128],int len){
                         print_dots(i,0);
                         print_stages(2,0,0);
                         print_stars(MIN(3,cycle-2-i));
-                        print_dots(7-i-MIN(3,cycle-2-i),0);
+                        print_dots(14-i-MIN(3,cycle-2-i),0);
                         printf("\n");
                     }
                 }
